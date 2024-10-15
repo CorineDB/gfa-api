49e5f596a0df5b6dea1c795e133cb44694a5bc2c
KANONTIN VENCESLAS <CELERITEHOLDING@GMAIL.COM>
2024-10-15 01:28:42 +0100

    commenter pta de authentification pour regler bug sigle

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -156,6 +156,7 @@ class AuthService extends BaseService implements AuthServiceInterface
 
             if((file_exists(storage_path('app')."/pta/pta.json") && Storage::disk('local')->get('pta/pta.json') == "") || !file_exists(storage_path('app')."/pta/pta.json"))
             {
+                //wens
                 // if(Auth::user()->programme) dispatch(new GenererPta(Auth::user()->programme))->delay(now()->addSeconds(3));
             }
 
