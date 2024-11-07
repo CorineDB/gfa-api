d44f26024d9d366d920a6b6d480a8eb3dd6c32c4
HÔKWÈ Holding <hokweholding@gmail.com>
2024-11-07 22:11:29 +0100

    Commit

--- app/Console/Commands/GenerateEvaluationResultats.php
+++ app/Console/Commands/GenerateEvaluationResultats.php
@@ -154,6 +154,7 @@ class GenerateEvaluationResultats extends Command
                         $query->where('questionId', $question_de_gouvernance->id);
                     }])->each(function($option_de_reponse) use(&$weighted_sum) {
 
+                        dump($option_de_reponse->pivot);
                         $note_i = $option_de_reponse->pivot->point ?? 0; // Default to 0 if there's no point
                         $nbre_i = $option_de_reponse->reponses_count ?? 0; // Default to 0 if there are no responses
 
