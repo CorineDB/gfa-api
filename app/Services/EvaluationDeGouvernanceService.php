6f096f60fa6d84b7c660867da97a26542ba1756a
HÔKWÈ Holding <hokweholding@gmail.com>
2024-11-19 14:15:45 +0100

    Test suppression d'une option de reponse.

--- app/Services/EvaluationDeGouvernanceService.php
+++ app/Services/EvaluationDeGouvernanceService.php
@@ -393,7 +393,7 @@ class EvaluationDeGouvernanceService extends BaseService implements EvaluationDe
                 $query->wherePivot('token', $token);
             }])->first())) throw new Exception("Evaluation de gouvernance inconnue.", 500);
 
-            $organisation = $evaluationDeGouvernance->organisations->get();
+            $organisation = $evaluationDeGouvernance->organisations;
             return response()->json(['statut' => 'success', 'message' => "Enregistrement réussir", 'data' => $organisation, 'statutCode' => 500], 500);
 
             if ($organisation != null) {
