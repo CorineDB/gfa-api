df57bec1fec825eaeaee8d576424f155d341c47e
HÔKWÈ Holding <hokweholding@gmail.com>
2024-11-25 14:44:29 +0100

    Charger les organisations responsables d'un indicatuer

--- app/Services/OrganisationService.php
+++ app/Services/OrganisationService.php
@@ -59,13 +59,6 @@ class OrganisationService extends BaseService implements OrganisationServiceInte
 
         try {
 
-
-            /*if (Auth::user()->hasRole('administrateur')) {
-                $organisations = $this->repository->all();
-            } else {
-                $organisations = Auth::user()->programme->organisations;
-            }*/
-
             $organisations = $this->repository->newInstance()::byProgramme()->get();
 
             return response()->json(['statut' => 'success', 'message' => null, 'data' => OrganisationResource::collection($organisations), 'statutCode' => Response::HTTP_OK], Response::HTTP_OK);
