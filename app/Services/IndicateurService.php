5a17e901e61f6eeddc35350c62bd2a3c03219271
HÔKWÈ Holding <hokweholding@gmail.com>
2024-11-25 15:04:03 +0100

    Charger les organisations responsables d'un indicatuer

--- app/Services/IndicateurService.php
+++ app/Services/IndicateurService.php
@@ -384,9 +384,6 @@ class IndicateurService extends BaseService implements IndicateurServiceInterfac
                     if(!($organisation = app(OrganisationRepository::class)->findById($organisation_responsable))) throw new Exception("Organisation inconnu", 1);
 
                     array_push($responsables, [$organisation->id => ["responsableable_type" => Organisation::class, "programmeId" => $attributs["programmeId"], "created_at" => now(), "updated_at" => now()]]);
-
-                    return response()->json(['statut' => 'success', 'message' => null, 'data' => $responsables, 'statutCode' => 500], 500);
-
                 }
 
                 $indicateur->organisations_responsable()->attach($responsables);
