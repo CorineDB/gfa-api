365411939df23020cd884ff83d99e487683350ef
HÔKWÈ Holding <hokweholding@gmail.com>
2024-12-05 01:36:28 +0100

    Generation des resultats d'une soumission.

--- app/Console/Commands/GenerateResultatsForValidatedSoumission.php
+++ app/Console/Commands/GenerateResultatsForValidatedSoumission.php
@@ -109,8 +109,6 @@ class GenerateResultatsForValidatedSoumission extends Command
                         $profile = ProfileDeGouvernance::create(['resultat_synthetique' => $results, 'evaluationOrganisationId' => $evaluationOrganisationId, 'evaluationDeGouvernanceId' => $evaluationDeGouvernance->id, 'organisationId' => $organisationId, 'programmeId' => $evaluationDeGouvernance->programmeId]);
                     }
 
-                    $this->info("Generated result for soumissions".$profile);
-
                 }
                 else if ($group_soumission === "perception") {
 
@@ -155,8 +153,6 @@ class GenerateResultatsForValidatedSoumission extends Command
 
                     $this->info("Generated result for soumissions".$profile);
                 }
-
-                $this->info("Generated result for soumissions".$profile);
             }
 
             if ($profile = $evaluationDeGouvernance->profiles($organisationId, $evaluationOrganisationId)->first()) {
