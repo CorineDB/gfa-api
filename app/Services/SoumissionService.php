1e331413190bb837d075822ef5267bc81c51256e
HÔKWÈ Holding <hokweholding@gmail.com>
2024-12-13 13:53:08 +0100

    Generation des resultats d'une soumission.

--- app/Services/SoumissionService.php
+++ app/Services/SoumissionService.php
@@ -106,7 +106,6 @@ class SoumissionService extends BaseService implements SoumissionServiceInterfac
 
                 $attributs = array_merge($attributs, ['formulaireDeGouvernanceId' => $formulaireDeGouvernance->id]);
             }
-            throw new Exception("Cette organisation n'est pas de cette evaluation.", Response::HTTP_NOT_FOUND);
 
             if (isset($attributs['organisationId'])) {
 
