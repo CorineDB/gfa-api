b60785b053d2d889e11e02a2d6801b8198a0011a
HÔKWÈ Holding <hokweholding@gmail.com>
2025-01-17 14:57:50 +0100

    Correction.

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -404,8 +404,6 @@ class AuthService extends BaseService implements AuthServiceInterface
 
             //event(new Login(Auth::user()->programme));
 
-            dd($data);
-
             // Retourner le token
             return response()->json(['statut' => 'success', 'message' => 'Authentification réussi', 'data' => new LoginResource($data), 'statutCode' => Response::HTTP_OK], Response::HTTP_OK)/*->withCookie('XSRF-TOKEN', $data['access_token'], 60*3)*/;
 
