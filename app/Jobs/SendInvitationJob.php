edd4d6048c69b883f76083dc68bae6c22b62fe72
HÔKWÈ Holding <hokweholding@gmail.com>
2025-01-24 17:22:33 +0100

    Correction.

--- app/Jobs/SendInvitationJob.php
+++ app/Jobs/SendInvitationJob.php
@@ -121,7 +121,8 @@ class SendInvitationJob implements ShouldQueue
                                     'content' => "Salut, Monsieur/Madame!\n\n".
                                                 "Vous etes invite(e) a participer a l'enquete d'auto-evaluation de gouvernance de {$evaluationOrganisation->user->nom} dans le cadre du programme {$this->evaluationDeGouvernance->programme->nom} - annee d'exercice {$this->evaluationDeGouvernance->annee_exercice}.\n\n".
                                                 "Cliquez des maintenant sur le lien ci-dessous pour acceder a l’enquete et partager votre precieuse opinion:\n".
-                                                "PARTICIPEZ DES MAINTENANT A L'ENQUETE: \n\n"
+                                                "PARTICIPEZ DES MAINTENANT A L'ENQUETE: \n\n".
+                                                "{$url}/dashboard/tools-perception/{$evaluationOrganisation->pivot->token},\n\n"
                                 ]
                             ]
                         ];
