cb2f34657fbb38ca53a81f8f292944b500d1dce5
HÔKWÈ Holding <hokweholding@gmail.com>
2025-02-01 00:07:06 +0100

    Correction.

--- app/Jobs/SendInvitationJob.php
+++ app/Jobs/SendInvitationJob.php
@@ -133,7 +133,6 @@ class SendInvitationJob implements ShouldQueue
                             $participants = $this->removeDuplicateParticipants(array_merge($participants, $this->data["participants"]), 'phone');
                             //return $response->json(); // or handle as needed
                         } else {
-                            dd($response->body());
                             $response->throw();
                             //return $response->body(); // Debug or log error
                             //throw new Exception("Error Processing Request", 1);
