8c6dd5bb6a0ba157d3e5cb156fe7ae2b396f3ccc
HÔKWÈ Holding <hokweholding@gmail.com>
2025-02-03 15:13:19 +0100

    Correction.

--- app/Services/EvaluationDeGouvernanceService.php
+++ app/Services/EvaluationDeGouvernanceService.php
@@ -1003,7 +1003,7 @@ class EvaluationDeGouvernanceService extends BaseService implements EvaluationDe
                     ];
                     
 
-                    $response = Http::withHeaders([
+                    $response = Http::dd()->withHeaders([
                         'Authorization' => "Basic {$this->sms_api_key}",
                         'Content-Type' => 'application/json',
                     ])->post($this->sms_api_url . '/sendbatch', $request_body);
