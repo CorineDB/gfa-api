02f97060ecef622e910861409d53dee880007e16
HÔKWÈ Holding <hokweholding@gmail.com>
2025-02-12 14:31:08 +0100

    Correction.

--- app/Models/Suivi.php
+++ app/Models/Suivi.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Support\Facades\DB;
 use SaiAshirwadInformatia\SecureIds\Models\Traits\HasSecureIds;
 
 class Suivi extends Model
@@ -23,121 +24,135 @@ class Suivi extends Model
     {
         parent::boot();
 
-        static::created(function ($suivi) {
+        static::creating(function ($suivi) {
+            DB::beginTransaction();
+            try {
 
-            $morphisme =  $suivi->suivitable;
+                $morphisme =  $suivi->suivitable;
 
-            if ($morphisme instanceof Tache) {
+                if ($morphisme instanceof Tache) {
 
-                $activite = $morphisme->activite;
+                    $activite = $morphisme->activite;
 
-                //if (in_array([0, 1], $activite->statut)) {
+                    //if (in_array([0, 1], $activite->statut)) {
 
-                    /* $totalPoids = $activite->taches->sum("poids");
+                        /* $totalPoids = $activite->taches->sum("poids");
 
-                    $totalPoidsActuel = $activite->taches->load('suivis')->pluck('suivis')->map(function ($suivi) {
-                        $lastPoid = $suivi->last();
-                        if ($lastPoid) {
-                            return $lastPoid->poidsActuel;
-                        }
-                        return 0;
-                    })->sum();
-
-                    $poidsActuel = ($activite->poids * $totalPoidsActuel) / $totalPoids; */
-
-                    $totalPoidsActuel = $activite->taches->each(function($tache){
-                        return $tache->suivi();
-                    })->sum();
-
-                    $poidsActuel = $totalPoidsActuel / $activite->taches->count();
-
-                    $activite->suivis()->create(["poidsActuel" => $poidsActuel]);
-
-                    $activite = $activite->fresh();
-                    /* 
-                    if ($activite->poids === $activite->poidsActuel)
-                        $activite->statuts()->create(['etat' => 2]);
-                    */
-
-
-                    if ($activite->poidsActuel === 100){
-                        $activite->statuts()->create(['etat' => 2]);
-                    }
-                //}
-
-            } elseif ($morphisme instanceof Activite) {
-
-                $composante = $morphisme->composante;
-
-                //if (in_array([0, 1], $composante->statut)) {
-
-                    $totalPoids = 0;
-
-                    $totalPoidsActuel = 0;
-
-                    $tacheCount = 0;
-
-                    if ($composante->composanteId !== null) {
-
-                        foreach ($composante->sousComposantes as $sousComposante) {
-                            foreach ($sousComposante->activites as $activite) {
-
-                                /* $totalPoids += $activite->taches->sum("poids");
-
-                                $totalPoidsActuel += $activite->taches->load('suivis')->pluck('suivis')->map(function ($suivi) {
-                                    $lastPoid = $suivi->last();
-                                    if ($lastPoid) {
-                                        return $lastPoid->poidsActuel;
-                                    }
-                                    return 0;
-                                })->sum(); */
-
-                                $totalPoidsActuel += $activite->taches->each(function($tache){
-                                    return $tache->suivi();
-                                })->sum();
-
-                                $tacheCount += $activite->taches->count();
-                            }
-                        }
-                    }
-
-                    foreach ($composante->activites as $activite) {
-
-                        /*$totalPoids += $activite->taches->sum("poids");
-
-                        $totalPoidsActuel += $activite->taches->load('suivis')->pluck('suivis')->map(function ($suivi) {
+                        $totalPoidsActuel = $activite->taches->load('suivis')->pluck('suivis')->map(function ($suivi) {
                             $lastPoid = $suivi->last();
                             if ($lastPoid) {
                                 return $lastPoid->poidsActuel;
                             }
                             return 0;
-                        })->sum();*/
+                        })->sum();
 
-                        $totalPoidsActuel += $activite->taches->each(function($tache){
+                        $poidsActuel = ($activite->poids * $totalPoidsActuel) / $totalPoids; */
+
+                        $totalPoidsActuel = $activite->taches->each(function($tache){
                             return $tache->suivi();
                         })->sum();
 
-                        $tacheCount += $activite->taches->count();
+                        $poidsActuel = $totalPoidsActuel / $activite->taches->count();
 
-                    }
+                        $activite->suivis()->create(["poidsActuel" => $poidsActuel]);
 
-                    //$poidsActuel = ($composante->poids * $totalPoidsActuel) / $totalPoids;
+                        $activite = $activite->fresh();
+                        /* 
+                        if ($activite->poids === $activite->poidsActuel)
+                            $activite->statuts()->create(['etat' => 2]);
+                        */
 
-                    $poidsActuel = $totalPoidsActuel / $tacheCount;
 
-                    $suivi = $composante->suivis()->create(["poidsActuel" => $poidsActuel]);
+                        if ($activite->poidsActuel === 100){
+                            $activite->statut = 2;
+                            $activite->save();
+                            $activite->statuts()->create(['etat' => 2]);
+                        }
+                    //}
 
-                    $composante = $composante->fresh();
+                } elseif ($morphisme instanceof Activite) {
 
-                    /* if ($composante->poids === $composante->poidsActuel){
-                        $composante->statuts()->create(['etat' => 2]);
-                    } */
+                    $composante = $morphisme->composante;
 
-                    if ($composante->poidsActuel === 100){
-                        $composante->statuts()->create(['etat' => 2]);
-                    }
-                //}
+                    //if (in_array([0, 1], $composante->statut)) {
 
+                        $totalPoids = 0;
+
+                        $totalPoidsActuel = 0;
+
+                        $tacheCount = 0;
+
+                        if ($composante->composanteId !== null) {
+
+                            foreach ($composante->sousComposantes as $sousComposante) {
+                                foreach ($sousComposante->activites as $activite) {
+
+                                    /* $totalPoids += $activite->taches->sum("poids");
+
+                                    $totalPoidsActuel += $activite->taches->load('suivis')->pluck('suivis')->map(function ($suivi) {
+                                        $lastPoid = $suivi->last();
+                                        if ($lastPoid) {
+                                            return $lastPoid->poidsActuel;
+                                        }
+                                        return 0;
+                                    })->sum(); */
+
+                                    $totalPoidsActuel += $activite->taches->each(function($tache){
+                                        return $tache->suivi();
+                                    })->sum();
+
+                                    $tacheCount += $activite->taches->count();
+                                }
+                            }
+                        }
+
+                        foreach ($composante->activites as $activite) {
+
+                            /*$totalPoids += $activite->taches->sum("poids");
+
+                            $totalPoidsActuel += $activite->taches->load('suivis')->pluck('suivis')->map(function ($suivi) {
+                                $lastPoid = $suivi->last();
+                                if ($lastPoid) {
+                                    return $lastPoid->poidsActuel;
+                                }
+                                return 0;
+                            })->sum();*/
+
+                            $totalPoidsActuel += $activite->taches->each(function($tache){
+                                return $tache->suivi();
+                            })->sum();
+
+                            $tacheCount += $activite->taches->count();
+
+                        }
+
+                        //$poidsActuel = ($composante->poids * $totalPoidsActuel) / $totalPoids;
+
+                        $poidsActuel = $totalPoidsActuel / $tacheCount;
+
+                        $suivi = $composante->suivis()->create(["poidsActuel" => $poidsActuel]);
+
+                        $composante = $composante->fresh();
+
+                        /* if ($composante->poids === $composante->poidsActuel){
+                            $composante->statuts()->create(['etat' => 2]);
+                        } */
+
+                        if ($composante->poidsActuel === 100){
+                            $composante->statut = 2;
+                            $composante->save();
+                            $composante->statuts()->create(['etat' => 2]);
+                        }
+                    //}
+
+                }
+
+                DB::commit();
+
+            } catch (\Throwable $th) {
+                DB::rollBack();
+
+                throw new Exception($th->getMessage(), 1);
             }
         });
     }
