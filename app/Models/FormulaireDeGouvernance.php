53a0a9a4f03fab06ee7e191c1136ed2e26af26d7
HÔKWÈ Holding <hokweholding@gmail.com>
2025-02-17 19:13:18 +0100

    Correction.

--- app/Models/FormulaireDeGouvernance.php
+++ app/Models/FormulaireDeGouvernance.php
@@ -30,8 +30,9 @@ class FormulaireDeGouvernance extends Model
         parent::boot();
 
         static::deleting(function ($formulaire_de_gouvernance) {
-            if (($formulaire_de_gouvernance->evaluations_de_gouvernance->count() > 0) || ($formulaire_de_gouvernance->options_de_reponse->count() > 0) || ($formulaire_de_gouvernance->questions_de_gouvernance->count() > 0) || ($formulaire_de_gouvernance->categories_de_gouvernance->count() > 0)) {
+            if ($formulaire_de_gouvernance->evaluations_de_gouvernance->count() > 0) {
                 // Prevent deletion by throwing an exception
+                throw new Exception("Impossible de supprimer ce formulaire de gouvernance. Veuillez d'abord supprimer toutes les évaluations associées.");
                 throw new Exception("Cannot delete because there are associated resource.");
             }
         });
