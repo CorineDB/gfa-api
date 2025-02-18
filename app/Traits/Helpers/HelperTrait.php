ed5b3d42246ea7e67ed7d5c9ba121d932cb611d7
HÔKWÈ Holding <hokweholding@gmail.com>
2025-02-18 17:49:07 +0100

    Correction.

--- app/Traits/Helpers/HelperTrait.php
+++ app/Traits/Helpers/HelperTrait.php
@@ -185,19 +185,12 @@ trait HelperTrait
         else {
             [$debutDate, $finDate] = $this->getCurrentTrimestreDates();
         }
-        
-        dd([$debutDate, $finDate]);
 
         // Check if there exists any duration where the task's dates fit within one of the activity's date ranges
         return $activite->durees()
             ->where(function($query) use ($debutDate, $finDate) {
                 $query->where('debut', '>=', $debutDate)
                       ->where('fin', '<=', $finDate);
-                // Check if the task's start date and end date fall within any of the ranges
-                /* $query->where(function ($subQuery) use ($debutDate, $finDate) {
-                    $subQuery->where('debut', '<=', $debutDate)
-                             ->where('fin', '>=', $finDate);
-                }); */
             })
             ->exists(); // Return true if such a range exists
     }
