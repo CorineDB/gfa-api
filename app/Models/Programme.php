7476dbf1c2ade4ade56be2d006bd858c7e970931
HÔKWÈ Holding <hokweholding@gmail.com>
2025-02-18 08:10:15 +0100

    Correction.

--- app/Models/Programme.php
+++ app/Models/Programme.php
@@ -526,6 +526,7 @@ class Programme extends Model
             $query->whereHas('composante', function ($query) {
                 $query->whereHas('projet', function ($query) {
                     $user = auth()->user();
+                    dd($user);
                     if ($user->profilable) {
                         /* $query->where("projetable_id", $user->profilable->id)
                               ->where("projetable_type", Organisation::class); */
