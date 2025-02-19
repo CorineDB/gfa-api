44e6eef2a7e20ffd610d0b1a19b3dac428824f8a
HÔKWÈ Holding <hokweholding@gmail.com>
2025-02-19 17:11:00 +0100

    Correction.

--- app/Http/Requests/user/UpdateUserRequest.php
+++ app/Http/Requests/user/UpdateUserRequest.php
@@ -31,7 +31,6 @@ class UpdateUserRequest extends FormRequest
         {
             $this->utilisateur = User::findByKey($this->utilisateur);
         }
-        dd($this->utilisateur);
         
         return [
             'nom' => 'sometimes|string|max:255',
