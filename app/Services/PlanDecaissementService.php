d21b77cd98b822cd9e2cac61916eba8d4f4144d1
HÔKWÈ Holding <hokweholding@gmail.com>
2025-03-05 16:36:18 +0100

    Correction.

--- app/Services/PlanDecaissementService.php
+++ app/Services/PlanDecaissementService.php
@@ -69,55 +69,107 @@ class PlanDecaissementService extends BaseService implements PlanDecaissementSer
         {
             $controle = 1;
 
-            if(!($activite = $this->activiteRepository->findById($attributs['activiteId']))) throw new Exception( "Cette activité n'existe pas", 500);
+            if (!($activite = $this->activiteRepository->findById($attributs['activiteId']))) {
+                throw new Exception("Cette activité n'existe pas", 404);
+            }
+    
             $attributs = array_merge($attributs, ['activiteId' => $activite->id]);
 
             $durees = $activite->durees;
-            foreach($durees as $duree)
-            {
+            $trimestreValide = false;
+            $validTrimestres = [];
+
+            foreach ($durees as $duree) {
                 $debutTab = explode('-', $duree->debut);
                 $finTab = explode('-', $duree->fin);
-
-                if($debutTab[0] <= $attributs['annee'] && $finTab[0] >= $attributs['annee'])
-                {
+    
+                if ($debutTab[0] <= $attributs['annee'] && $finTab[0] >= $attributs['annee']) {
                     $controle = 0;
-                    break;
+    
+                    // Vérification du trimestre couvert par la durée
+                    // Convert months to trimestres
+                    $moisDebut = (int) $debutTab[1];
+                    $moisFin = (int) $finTab[1];
+    
+                    $trimestreDebut = ceil($moisDebut / 3);
+                    $trimestreFin = ceil($moisFin / 3);
+    
+                    for ($i = $trimestreDebut; $i <= $trimestreFin; $i++) {
+                        $validTrimestres[] = $i;
+                    }
+
+                    if ($attributs['trimestre'] >= $trimestreDebut && $attributs['trimestre'] <= $trimestreFin) {
+                        $trimestreValide = true;
+                    }
+                    //break;
                 }
             }
 
-            if($controle)
-                throw new Exception("L'activite n'a aucune durée d'execution dans l'année precisé", 500);
 
+            if ($controle) {
+                throw new Exception("L'activité n'a aucune durée d'exécution dans l'année précisée", 500);
+            }
 
-            $trimestres = $this->repository->allFiltredBy([['attribut' => 'activiteId', 'operateur' => '=', 'valeur' => $attributs['activiteId']],
-                                                      ['attribut' => 'annee', 'operateur' => '=', 'valeur' => $attributs['annee']]])
-                                           ->pluck('trimestre');
+            if (!$trimestreValide) {
+                throw new Exception("Le trimestre sélectionné ne correspond pas à la période d'exécution de l'activité", 500);
+            }
 
-            if(!(count($trimestres)) && $attributs['trimestre'] != 1)
-                throw new Exception("Vous devez d'abord faire le plan du trimestre 1", 500);
+            // Get registered trimestres for this activity in the selected year
+            $trimestres = $this->repository
+                ->allFiltredBy([
+                    ['attribut' => 'activiteId', 'operateur' => '=', 'valeur' => $attributs['activiteId']],
+                    ['attribut' => 'annee', 'operateur' => '=', 'valeur' => $attributs['annee']]
+                ])
+                ->pluck('trimestre');
 
-
-            $nombreDeTrimestre = count($trimestres);
-            if($nombreDeTrimestre === 0) $max = 0;
-            else $max = max($trimestres->all());
-
-            if($nombreDeTrimestre == 0 && $attributs['trimestre'] > 1) throw new Exception("Vous devez d'abord faire le plan du premier trimestre", 500);
-
-            else if( $nombreDeTrimestre == 0 && $attributs['trimestre'] == 1 );
-
-            else
-            {
-                if($attributs['trimestre'] <= $max)
-                    throw new Exception("Le plan du trimestre {$attributs['trimestre']} a déjà été effectué", 500);
-
-                else if($attributs['trimestre'] > $max+1)
-                {
-                    $max = $max+1;
-                    throw new Exception("Vous devez d'abord faire le plan du trimestre {$max}", 500);
+            /*
+                if(!(count($trimestres)) && $attributs['trimestre'] != 1){
+                    throw new Exception("Vous devez d'abord faire le plan du trimestre 1", 500);
                 }
 
-                else if( $nombreDeTrimestre + 1 > 4)
-                    throw new Exception("Le plan ne peut qu'être faire sur les 4 trimestres d'une année.", 1);
+                $nombreDeTrimestre = count($trimestres);
+
+                if($nombreDeTrimestre === 0){
+                    $max = 0;
+                }
+                else{
+                    $max = max($trimestres->all());
+                }
+                    
+                if($nombreDeTrimestre == 0 && $attributs['trimestre'] > 1){
+                    throw new Exception("Vous devez d'abord faire le plan du premier trimestre", 500);
+                }
+
+                else if( $nombreDeTrimestre == 0 && $attributs['trimestre'] == 1 );
+
+                else
+                {
+                    if($attributs['trimestre'] <= $max){
+                        throw new Exception("Le plan du trimestre {$attributs['trimestre']} a déjà été effectué", 500);
+                    }
+                    else if($attributs['trimestre'] > $max+1)
+                    {
+                        $max = $max+1;
+                        throw new Exception("Vous devez d'abord faire le plan du trimestre {$max}", 500);
+                    }
+
+                    else if( $nombreDeTrimestre + 1 > 4)
+                        throw new Exception("Le plan ne peut qu'être faire sur les 4 trimestres d'une année.", 1);
+                }
+            */
+
+            $registeredTrimestres = $trimestres->all();
+
+            // Allow only if the selected trimestre is within valid execution periods
+            if (!in_array($attributs['trimestre'], $validTrimestres)) {
+                throw new Exception("L'activité ne peut pas être exécutée pour le trimestre {$attributs['trimestre']}", 500);
+            }
+
+            // Allow skipping trimestres if execution periods allow it
+            $maxRegistered = empty($registeredTrimestres) ? 0 : max($registeredTrimestres);
+
+            if ($maxRegistered > 0 && $attributs['trimestre'] > $maxRegistered + 1) {
+                throw new Exception("Vous devez d'abord enregistrer le trimestre ". $maxRegistered + 1 ." avant d'ajouter T". $attributs['trimestre'], 500);
             }
 
             $planDecaissement = $this->repository->create(array_merge($attributs, ['programmeId' => auth()->user()->programmeId]));
