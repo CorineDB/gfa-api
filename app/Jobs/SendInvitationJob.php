ce70c217e8e57539d604604ed722a0750451cf60
HÔKWÈ Holding <hokweholding@gmail.com>
2025-03-09 15:49:17 +0100

    Correction.

--- app/Jobs/SendInvitationJob.php
+++ app/Jobs/SendInvitationJob.php
@@ -131,8 +131,11 @@ class SendInvitationJob implements ShouldQueue
                     if(isset($this->data['nbreParticipants'])){
                         if($this->data['nbreParticipants'] > 0){
 
-                            Log::info('nbreParticipants: ' . $this->data['nbreParticipants'] ."; pivot->nbreParticipants ". $evaluationOrganisation->pivot->nbreParticipants . "; total_soumissions_de_perception: " . $this->evaluationDeGouvernance->total_soumissions_de_perception);
-                            Log::info("nbreParticipants < pivot->nbreParticipants " . $this->data['nbreParticipants'] < $evaluationOrganisation->pivot->nbreParticipants . "; nbreParticipants < total_soumissions_de_perception " . ($this->data['nbreParticipants'] >= $this->evaluationDeGouvernance->total_soumissions_de_perception));
+                            Log::info('nbreParticipants: ' . $this->data['nbreParticipants'] ."; pivot->nbreParticipants: ". $evaluationOrganisation->pivot->nbreParticipants . "; total_soumissions_de_perception: " . $this->evaluationDeGouvernance->total_soumissions_de_perception);
+                            Log::info("nbreParticipants < pivot->nbreParticipants: " . $this->data['nbreParticipants'] < $evaluationOrganisation->pivot->nbreParticipants . "; nbreParticipants < total_soumissions_de_perception: " . ($this->data['nbreParticipants'] >= $this->evaluationDeGouvernance->total_soumissions_de_perception));
+
+                            Log::info("if condition: " . (($this->data['nbreParticipants'] < $evaluationOrganisation->pivot->nbreParticipants) && ($this->data['nbreParticipants'] >= $this->evaluationDeGouvernance->total_soumissions_de_perception)));
+
                             if(($this->data['nbreParticipants'] < $evaluationOrganisation->pivot->nbreParticipants) && ($this->data['nbreParticipants'] >= $this->evaluationDeGouvernance->total_soumissions_de_perception)){
                                 $evaluationOrganisation->pivot->nbreParticipants = $this->data['nbreParticipants'];
                                 $evaluationOrganisation->pivot->save();
