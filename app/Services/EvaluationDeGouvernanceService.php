17beeb4e14b2ca7681bf52eb833b5b4ce2c02877
HÔKWÈ Holding <hokweholding@gmail.com>
2025-03-16 21:55:04 +0100

    Correction.

--- app/Services/EvaluationDeGouvernanceService.php
+++ app/Services/EvaluationDeGouvernanceService.php
@@ -959,6 +959,8 @@ class EvaluationDeGouvernanceService extends BaseService implements EvaluationDe
                 return response()->json(['statut' => 'error', 'message' => "Pas le droit", 'data' => null, 'statutCode' => Response::HTTP_FORBIDDEN], Response::HTTP_FORBIDDEN);
             }
 
+            dd($attributs);
+
             SendInvitationJob::dispatch($evaluationDeGouvernance, $attributs, 'invitation-enquete-de-collecte')->delay(30);
 
             return response()->json(['statut' => 'success', 'message' => "Invitation envoye", 'data' => null, 'statutCode' => Response::HTTP_OK], Response::HTTP_OK);
