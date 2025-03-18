509a98813246fb84d36c4258944dcc9f9ba16eef
HÔKWÈ Holding <hokweholding@gmail.com>
2025-03-18 18:40:58 +0100

    Correction.

--- app/Http/Requests/formulaires_de_gouvernance/UpdateRequest.php
+++ app/Http/Requests/formulaires_de_gouvernance/UpdateRequest.php
@@ -66,7 +66,7 @@ class UpdateRequest extends FormRequest
             //'factuel.types_de_gouvernance.*.principes_de_gouvernance.*.criteres_de_gouvernance.*.position' => ["required", new DistinctAttributeRule(), "min:1"],
             'factuel.types_de_gouvernance.*.principes_de_gouvernance.*.criteres_de_gouvernance.*.indicateurs_de_gouvernance' => ["required", "array", "min:1"],
             'factuel.types_de_gouvernance.*.principes_de_gouvernance.*.criteres_de_gouvernance.*.indicateurs_de_gouvernance.*' => ["sometimes", function($attribute, $value, $fail){
-                $fail( $attribute . ' ' . $value);
+                dd($value);
             }, new DistinctAttributeRule(), /* "distinct",  */new HashValidatorRule(new IndicateurDeGouvernance())],
             'factuel.types_de_gouvernance.*.principes_de_gouvernance.*.criteres_de_gouvernance.*.indicateurs_de_gouvernance.*.id' => ["sometimes", new DistinctAttributeRule(), new HashValidatorRule(new IndicateurDeGouvernance())],
 
