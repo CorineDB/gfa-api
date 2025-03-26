112ad1153c2ee43d1f70cd18caef8bd0d44a06c8
HÔKWÈ Holding <hokweholding@gmail.com>
2025-03-26 19:46:06 +0100

    Correction.

--- app/Services/FormulaireDeGouvernanceService.php
+++ app/Services/FormulaireDeGouvernanceService.php
@@ -470,6 +470,16 @@ class FormulaireDeGouvernanceService extends BaseService implements FormulaireDe
 
                         }
 
+                        dd(array_values($questions_de_gouvernance));/* 
+                        $questions = [];
+
+                        // Fix: Flatten the array before using sync()
+                        foreach ($questions_de_gouvernance as $categorieId => $questionsOp) {
+                            array_push($questions, [$categorieId=>$questionsOp]);
+                        }
+
+                        dd($questions); */
+
                         $formulaireDeGouvernance->categorie_de_gouvernance()->sync($questions_de_gouvernance);
                     }
 
