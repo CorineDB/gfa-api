cc2016e6ab9a4f7ad101acdca44a08c758423125
HÔKWÈ Holding <hokweholding@gmail.com>
2025-03-29 18:10:05 +0100

    Correction.

--- app/Services/FormulaireDeGouvernanceService.php
+++ app/Services/FormulaireDeGouvernanceService.php
@@ -393,7 +393,7 @@ class FormulaireDeGouvernanceService extends BaseService implements FormulaireDe
                         }
                     }
 
-                    dd([$categories_de_gouvernance, $formulaireDeGouvernance->categorie_de_gouvernance]);
+                    dd([$categories_de_gouvernance, $formulaireDeGouvernance->all_categories_de_gouvernance]);
 
 
                     $categories_de_gouvernance = $formulaireDeGouvernance->categories_de_gouvernance()->whereNotIn('id', $categories_de_gouvernance);
