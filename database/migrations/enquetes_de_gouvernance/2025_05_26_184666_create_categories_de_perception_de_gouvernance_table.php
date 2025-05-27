7fab799689c4fe0fc6caaaebb0067f9ce378c5b8
HÔKWÈ Holding <hokweholding@gmail.com>
2025-05-28 00:43:47 +0200

    Module enquete de gouvernance

--- database/migrations/enquetes_de_gouvernance/2025_05_26_184666_create_categories_de_perception_de_gouvernance_table.php
+++ database/migrations/enquetes_de_gouvernance/2025_05_26_184666_create_categories_de_perception_de_gouvernance_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesPerceptionDeGouvernanceTable extends Migration
+class CreateCategoriesDePerceptionDeGouvernanceTable extends Migration
 {
     /**
      * Run the migrations.
