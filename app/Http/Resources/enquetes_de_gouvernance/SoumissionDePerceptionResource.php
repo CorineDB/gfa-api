bb3c363dd50f1edad0556bb37b8bea5ccc754a58
HÔKWÈ Holding <hokweholding@gmail.com>
2025-05-29 22:57:10 +0200

    Module enquetes_de_gouvernance

--- app/Http/Resources/enquetes_de_gouvernance/SoumissionDePerceptionResource.php
+++ app/Http/Resources/enquetes_de_gouvernance/SoumissionDePerceptionResource.php
@@ -41,9 +41,9 @@ class SoumissionDePerceptionResource extends JsonResource
 
             $questions = [];
 
-            if($sousCategorieDeGouvernance->questions_de_gouvernance->count()){
+            /* if($sousCategorieDeGouvernance->questions_de_gouvernance->count()){
                 $questions = ['questions_de_gouvernance' =>  $this->questions_reponses($sousCategorieDeGouvernance->questions_de_gouvernance)];
-            }
+            } */
 
             return array_merge([
                 'id' => $sousCategorieDeGouvernance->secure_id,
