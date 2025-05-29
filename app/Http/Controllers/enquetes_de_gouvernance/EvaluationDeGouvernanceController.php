719b1e005c57c3d1b488a70e41c9e83a6ad295e2
HÔKWÈ Holding <hokweholding@gmail.com>
2025-05-29 13:03:42 +0200

    Module enquete de gouvernance

--- app/Http/Controllers/enquetes_de_gouvernance/EvaluationDeGouvernanceController.php
+++ app/Http/Controllers/enquetes_de_gouvernance/EvaluationDeGouvernanceController.php
@@ -98,6 +98,16 @@ class EvaluationDeGouvernanceController extends Controller
         return $this->evaluationDeGouvernanceService->deleteById($id);
     }
 
+    public function organisations($id)
+    {
+        return $this->evaluationDeGouvernanceService->organisations($id);
+    }
+
+    public function soumissions($id)
+    {
+        return $this->evaluationDeGouvernanceService->soumissions($id);
+    }
+
 
     public function recommandations($id)
     {
