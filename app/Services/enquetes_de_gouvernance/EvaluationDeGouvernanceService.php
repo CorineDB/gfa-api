0aabad6883760bc80bd568759cb60bf9f4bcf764
HÔKWÈ Holding <hokweholding@gmail.com>
2025-05-29 22:40:32 +0200

    Module enquetes_de_gouvernance

--- app/Services/enquetes_de_gouvernance/EvaluationDeGouvernanceService.php
+++ app/Services/enquetes_de_gouvernance/EvaluationDeGouvernanceService.php
@@ -1097,7 +1097,7 @@ class EvaluationDeGouvernanceService extends BaseService implements EvaluationDe
                     }
                 } else {
 
-                    dd( $evaluationDeGouvernance->formulaire_de_perception_de_gouvernance());
+                    dd( $evaluationDeGouvernance->formulaires_de_perception_de_gouvernance);
 
                     $attributs = [
                         'evaluationId' => $evaluationDeGouvernance->id,
