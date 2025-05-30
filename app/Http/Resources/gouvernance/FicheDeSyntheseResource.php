c722161b1fc683fb11ed63f939bf65db20611d50
HÔKWÈ Holding <hokweholding@gmail.com>
2025-05-30 15:20:33 +0200

    Module enquete de gouvernance

--- app/Http/Resources/gouvernance/FicheDeSyntheseResource.php
+++ app/Http/Resources/gouvernance/FicheDeSyntheseResource.php
@@ -30,9 +30,9 @@ class FicheDeSyntheseResource extends JsonResource
             //'resultats'      => $this->when($this->type == 'factuel', $this->resultats),
             //'synthese'      => $this->synthese,
             'evaluatedAt'   => Carbon::parse($this?->evaluatedAt)->format("Y-m-d"),
-            'formulaireDeGouvernanceId' => $this?->formulaireDeGouvernance->secure_id,
-            'organisationId' => $this?->organisation->secure_id,
-            'evaluationDeGouvernanceId' => $this?->evaluation_de_gouvernance->secure_id
+            'formulaireDeGouvernanceId' => $this?->formulaireDeGouvernance?->secure_id,
+            'organisationId' => $this?->organisation?->secure_id,
+            'evaluationDeGouvernanceId' => $this?->evaluation_de_gouvernance?->secure_id
             /*
             'soumission'    => [
                 'id'                    => $this->soumission->secure_id,
