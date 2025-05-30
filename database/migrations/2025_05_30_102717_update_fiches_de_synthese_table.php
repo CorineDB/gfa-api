152ae791ce5cf9d6a88d5130f4ecba532e7f71d5
HÔKWÈ Holding <hokweholding@gmail.com>
2025-05-30 12:51:53 +0200

    Module enquete de gouvernance

--- database/migrations/2025_05_30_102717_update_fiches_de_synthese_table.php
+++ database/migrations/2025_05_30_102717_update_fiches_de_synthese_table.php
@@ -66,7 +66,8 @@ class UpdateFichesDeSyntheseTable extends Migration
      */
     public function down()
     {
-        Schema::table('fiches_de_synthese', function (Blueprint $table) {
+        Schema::dropIfExists('fiches_de_synthese');
+        /* Schema::table('fiches_de_synthese', function (Blueprint $table) {
             // Remove polymorphic columns
             $table->dropMorphs('formulaireDeGouvernance');
 
@@ -75,6 +76,6 @@ class UpdateFichesDeSyntheseTable extends Migration
             $table->foreign('formulaireDeGouvernanceId')
                 ->references('id')->on('formulaires_de_gouvernance') // or your original table
                 ->onDelete('cascade');
-        });
+        }); */
     }
 }
