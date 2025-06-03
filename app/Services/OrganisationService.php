54291d3d080d0ef1360aafaed6b4ee3dbed5763e
HÔKWÈ Holding <hokweholding@gmail.com>
2025-06-03 09:14:10 +0200

    Correction

--- app/Services/OrganisationService.php
+++ app/Services/OrganisationService.php
@@ -108,7 +108,7 @@ class OrganisationService extends BaseService implements OrganisationServiceInte
                 $attributs['latitude'] = "6.90000";
             }
 
-            dd((empty($attributs['longitude']) || $attributs['longitude'] == null));
+            dd(isset($attributs['longitude']) );
 
             $organisation = $this->repository->create($attributs);
 
