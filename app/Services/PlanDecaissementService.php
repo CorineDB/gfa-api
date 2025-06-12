4598ad1f49cdb79ae9ba40dda2dda37b9fc7e299
HÔKWÈ Holding <hokweholding@gmail.com>
2025-06-12 22:44:51 +0200

    Correction

--- app/Services/PlanDecaissementService.php
+++ app/Services/PlanDecaissementService.php
@@ -11,7 +11,6 @@ use App\Repositories\PlanDecaissementRepository;
 use App\Traits\Helpers\LogActivity;
 use Core\Services\Contracts\BaseService;
 use Core\Services\Interfaces\PlanDecaissementServiceInterface;
-use DateTime;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
@@ -20,9 +19,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 /**
- * Interface PlanDecaissementServiceInterface
- * @package Core\Services\Interfaces
- */
+* Interface PlanDecaissementServiceInterface
+* @package Core\Services\Interfaces
+*/
 class PlanDecaissementService extends BaseService implements PlanDecaissementServiceInterface
 {
 
@@ -62,11 +61,12 @@ class PlanDecaissementService extends BaseService implements PlanDecaissementSer
         }
     }
 
-    public function create(array $attributs): JsonResponse
+    public function create(array $attributs) : JsonResponse
     {
         DB::beginTransaction();
 
-        try {
+        try
+        {
             $controle = 1;
 
             if (!($activite = $this->activiteRepository->findById($attributs['activiteId']))) {
@@ -79,33 +79,7 @@ class PlanDecaissementService extends BaseService implements PlanDecaissementSer
             $trimestreValide = false;
             $validTrimestres = [];
 
-
-            foreach ($activite->durees as $duree) {
-
-                $debut = new DateTime($duree->debut);
-                $fin = new DateTime($duree->fin);
-
-                $anneeDebut = (int) $debut->format('Y');
-                $anneeFin = (int) $fin->format('Y');
-
-                if ($anneeDebut <= $attributs['annee'] && $anneeFin >= $attributs['annee']) {
-                    $controle = 0;
-
-                    // Début et fin dans l’année ciblée
-                    $startMonth = ($anneeDebut < $attributs['annee']) ? 1 : (int) $debut->format('m');
-                    $endMonth = ($anneeFin > $attributs['annee']) ? 12 : (int) $fin->format('m');
-
-                    $trimestreDebut = ceil($startMonth / 3);
-                    $trimestreFin = ceil($endMonth / 3);
-
-                    if ($attributs['trimestre'] >= $trimestreDebut && $attributs['trimestre'] <= $trimestreFin) {
-                        $trimestreValide = true;
-                        break;
-                    }
-                }
-            }
-
-            /* foreach ($durees as $duree) {
+            foreach ($durees as $duree) {
                 $debutTab = explode('-', $duree->debut);
                 $finTab = explode('-', $duree->fin);
 
@@ -126,10 +100,10 @@ class PlanDecaissementService extends BaseService implements PlanDecaissementSer
 
                     if ($attributs['trimestre'] >= $trimestreDebut && $attributs['trimestre'] <= $trimestreFin) {
                         $trimestreValide = true;
+                        break;
                     }
-                    break;
                 }
-            } */
+            }
 
 
             if ($controle) {
@@ -137,7 +111,7 @@ class PlanDecaissementService extends BaseService implements PlanDecaissementSer
             }
 
             if (!$trimestreValide) {
-                throw new Exception("Le trimestre sélectionné ne correspond pas à la période d'exécution de l'activité " . json_encode($durees->pluck('debut')->toArray()), 500);
+                throw new Exception("Le trimestre sélectionné ne correspond pas à la période d'exécution de l'activité", 500);
             }
 
             // Get registered trimestres for this activity in the selected year
@@ -188,19 +162,19 @@ class PlanDecaissementService extends BaseService implements PlanDecaissementSer
 
             // Allow only if the selected trimestre is within valid execution periods
             if (!in_array($attributs['trimestre'], $validTrimestres)) {
-                throw new Exception("L'activité ne peut pas être exécutée pour le trimestre {$attributs['trimestre']}", 500);
+                throw new Exception("L'activité ne peut pas être exécutée pour le trimestre {$attributs['trimestre']} $validTrimestres", 500);
             }
 
             // Allow skipping trimestres if execution periods allow it
             $maxRegistered = empty($registeredTrimestres) ? 0 : max($registeredTrimestres);
 
             if ($maxRegistered > 0 && $attributs['trimestre'] > $maxRegistered + 1) {
-                throw new Exception("Vous devez d'abord enregistrer le trimestre " . $maxRegistered + 1 . " avant d'ajouter T" . $attributs['trimestre'], 500);
+                throw new Exception("Vous devez d'abord enregistrer le trimestre ". $maxRegistered + 1 ." avant d'ajouter T". $attributs['trimestre'], 500);
             }
 
             $planDecaissement = $this->repository->create(array_merge($attributs, ['programmeId' => auth()->user()->programmeId]));
 
-            $acteur = Auth::check() ? Auth::user()->nom . " " . Auth::user()->prenom : "Inconnu";
+            $acteur = Auth::check() ? Auth::user()->nom . " ". Auth::user()->prenom : "Inconnu";
 
             $message = $message ?? Str::ucfirst($acteur) . " a créé un " . strtolower(class_basename($planDecaissement));
 
@@ -211,51 +185,54 @@ class PlanDecaissementService extends BaseService implements PlanDecaissementSer
             GenererPta::dispatch(Auth::user()->programme)->delay(5);
 
             return response()->json(['statut' => 'success', 'message' => null, 'data' => new PlansDecaissementResource($planDecaissement), 'statutCode' => Response::HTTP_OK], Response::HTTP_OK);
-        } catch (\Throwable $th) {
+        }
+        catch (\Throwable $th)
+        {
             DB::rollback();
             return response()->json(['statut' => 'error', 'message' => $th->getMessage(), 'errors' => []], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    public function update($planDecaissementId, array $attributs): JsonResponse
+    public function update($planDecaissementId, array $attributs) : JsonResponse
     {
         DB::beginTransaction();
 
-        try {
+        try
+        {
 
-            if (!($activite = $this->activiteRepository->findById($attributs['activiteId']))) throw new Exception("Cette activité n'existe pas", 500);
+            if(!($activite = $this->activiteRepository->findById($attributs['activiteId']))) throw new Exception( "Cette activité n'existe pas", 500);
             $attributs = array_merge($attributs, ['activiteId' => $activite->id]);
 
             $controle = 1;
 
             $durees = $activite->durees;
-            foreach ($durees as $duree) {
+            foreach($durees as $duree)
+            {
                 $debutTab = explode('-', $duree->debut);
                 $finTab = explode('-', $duree->fin);
 
-                if ($debutTab[0] <= $attributs['annee'] && $finTab[0] >= $attributs['annee']) {
+                if($debutTab[0] <= $attributs['annee'] && $finTab[0] >= $attributs['annee'])
+                {
                     $controle = 0;
                     break;
                 }
             }
 
-            if ($controle)
+            if($controle)
                 throw new Exception("L'activite n'a aucune durée d'execution dans l'année precisé", 500);
 
 
-            if (!($plan = $this->repository->findById($planDecaissementId))) throw new Exception("Cet plan n'existe pas", 500);
+            if(!($plan = $this->repository->findById($planDecaissementId))) throw new Exception( "Cet plan n'existe pas", 500);
 
-            $trimestres = $this->repository->allFiltredBy([
-                ['attribut' => 'activiteId', 'operateur' => '=', 'valeur' => $attributs['activiteId']],
-                ['attribut' => 'annee', 'operateur' => '=', 'valeur' => $attributs['annee']]
-            ])
-                ->pluck('trimestre');
+            $trimestres = $this->repository->allFiltredBy([['attribut' => 'activiteId', 'operateur' => '=', 'valeur' => $attributs['activiteId']],
+                                                         ['attribut' => 'annee', 'operateur' => '=', 'valeur' => $attributs['annee']]])
+                                           ->pluck('trimestre');
 
-            if (!(count($trimestres)) && $attributs['trimestre'] != 1)
+            if(!(count($trimestres)) && $attributs['trimestre'] != 1)
                 throw new Exception("Vous devez d'abord faire le plan du trimestre 1", 500);
 
 
-            if (!($plan->trimestre == $attributs['trimestre'])) throw new Exception("Vous pouvez pas modifier le trimestre", 500);
+            if(!($plan->trimestre == $attributs['trimestre'])) throw new Exception("Vous pouvez pas modifier le trimestre", 500);
 
             $planDecaissement = $this->repository->findById($planDecaissementId);
 
@@ -263,7 +240,7 @@ class PlanDecaissementService extends BaseService implements PlanDecaissementSer
 
             $planDecaissement->save();
 
-            $acteur = Auth::check() ? Auth::user()->nom . " " . Auth::user()->prenom : "Inconnu";
+            $acteur = Auth::check() ? Auth::user()->nom . " ". Auth::user()->prenom : "Inconnu";
 
             $message = $message ?? Str::ucfirst($acteur) . " a modifié un " . strtolower(class_basename($planDecaissement));
 
@@ -276,7 +253,9 @@ class PlanDecaissementService extends BaseService implements PlanDecaissementSer
             GenererPta::dispatch(Auth::user()->programme)->delay(5);
 
             return response()->json(['statut' => 'success', 'message' => null, 'data' =>  new PlansDecaissementResource($planDecaissement), 'statutCode' => Response::HTTP_OK], Response::HTTP_OK);
-        } catch (\Throwable $th) {
+        }
+        catch (\Throwable $th)
+        {
             DB::rollback();
             return response()->json(['statut' => 'error', 'message' => $th->getMessage(), 'errors' => []], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
