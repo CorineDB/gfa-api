1ea3a4f64d5a13e1f14951d24feb3c8cc1e8edd2
HÔKWÈ Holding <hokweholding@gmail.com>
2025-06-17 11:25:12 +0200

    Correction

--- app/Models/Indicateur.php
+++ app/Models/Indicateur.php
@@ -189,7 +189,7 @@ class Indicateur extends Model
     public function valeurCibleTotal()
     {
 
-        return $totals = [];
+        $totals = [];
 
         $this->valeursCible->pluck("valeurCible")->each(function ($item) use (&$totals) {
             foreach ($item as $key => $value) {
