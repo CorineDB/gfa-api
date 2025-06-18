a334cb73709832b3c6a0f7c6a7d764de0359674b
HÔKWÈ Holding <hokweholding@gmail.com>
2025-06-19 00:05:14 +0200

    Correction

--- app/Http/Controllers/enquetes_de_gouvernance/RecommandationController.php
+++ app/Http/Controllers/enquetes_de_gouvernance/RecommandationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\enquetes_de_gouvernance;
+namespace App\Http\Controllers\enquetes_de_gouvernance;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\recommandations\StoreRequest;
