77564543ebaf0060e2eda589a53628fdc3c75a79
HÔKWÈ Holding <hokweholding@gmail.com>
2025-10-02 17:39:24 +0200

    Commit

--- app/Services/enquetes_de_gouvernance/SoumissionFactuelService.php
+++ app/Services/enquetes_de_gouvernance/SoumissionFactuelService.php
@@ -165,13 +165,13 @@ class SoumissionFactuelService extends BaseService implements SoumissionFactuelS
 
                         $sourceDeVerification = SourceDeVerification::findById($item['sourceDeVerificationId']);
 
-                        throw new Exception("Error Processing Request" . json_encode($sourceDeVerification), 500);
-
                         if(!$sourceDeVerification) {
                             throw new Exception("Source de verification inconnue.", Response::HTTP_NOT_FOUND);
                         }
 
-                        if ($sourceDeVerification && $sourceDeVerification->programmeId == $programme->id) throw new Exception("Source de verification inconnue du programme.", Response::HTTP_NOT_FOUND);
+                        if ($sourceDeVerification && $sourceDeVerification->programmeId == $programme->id) {
+                            throw new Exception("Source de verification inconnue du programme.", Response::HTTP_NOT_FOUND);
+                        }
 
                         /*if (!(($sourceDeVerification = app(SourceDeVerificationRepository::class)->findById($item['sourceDeVerificationId'])) && optional($sourceDeVerification)->programmeId == $programme->id)) {
                             throw new Exception("Source de verification inconnue du programme.", Response::HTTP_NOT_FOUND);
