3bbf52b16ce2c8cc8cfc1724a3096822555ca4ac
HÔKWÈ Holding <hokweholding@gmail.com>
2025-10-02 16:03:59 +0200

    Commit

--- app/Services/enquetes_de_gouvernance/SoumissionFactuelService.php
+++ app/Services/enquetes_de_gouvernance/SoumissionFactuelService.php
@@ -161,14 +161,14 @@ class SoumissionFactuelService extends BaseService implements SoumissionFactuelS
                     if (!$option && $option->programmeId == $programme->id) throw new Exception("Cette option n'est pas dans le programme", Response::HTTP_NOT_FOUND);
 
                     if (isset($item['sourceDeVerificationId']) && (!empty($item['sourceDeVerificationId'])) && $item['sourceDeVerificationId'] != 'null') {
-                        throw new Exception("Error Processing Request : " . $item['sourceDeVerificationId'], 1);
+                        //throw new Exception("Error Processing Request : " . $item['sourceDeVerificationId'], 1);
 
-                        $sourceDeVerification = app(SourceDeVerificationRepository::class)->findById($item['sourceDeVerificationId']);
-                        if (!$sourceDeVerification && $sourceDeVerification->programmeId == $programme->id) throw new Exception("Source de verification inconnue du programme.", Response::HTTP_NOT_FOUND);
+                        //$sourceDeVerification = app(SourceDeVerificationRepository::class)->findByKey($item['sourceDeVerificationId']);
+                        //if (!$sourceDeVerification && $sourceDeVerification->programmeId == $programme->id) throw new Exception("Source de verification inconnue du programme.", Response::HTTP_NOT_FOUND);
 
-                        /*if (!(($sourceDeVerification = app(SourceDeVerificationRepository::class)->findById($item['sourceDeVerificationId'])) && optional($sourceDeVerification)->programmeId == $programme->id)) {
+                        if (!(($sourceDeVerification = app(SourceDeVerificationRepository::class)->findByKey($item['sourceDeVerificationId'])) && optional($sourceDeVerification)->programmeId == $programme->id)) {
                             throw new Exception("Source de verification inconnue du programme.", Response::HTTP_NOT_FOUND);
-                        }*/
+                        }
 
                         $item = array_merge($item, ['sourceDeVerificationId' => $sourceDeVerification->id, 'sourceDeVerification' => null]);
                     } else if (isset($item['sourceDeVerification']) && (!empty($item['sourceDeVerification'])) && $item['sourceDeVerification'] != 'null') {
