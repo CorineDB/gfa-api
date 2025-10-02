0235f1b1d9758512d75703c3f4719d6348b0c69e
HÔKWÈ Holding <hokweholding@gmail.com>
2025-10-02 12:48:57 +0200

    Commit

--- app/Services/enquetes_de_gouvernance/SoumissionFactuelService.php
+++ app/Services/enquetes_de_gouvernance/SoumissionFactuelService.php
@@ -125,14 +125,14 @@ class SoumissionFactuelService extends BaseService implements SoumissionFactuelS
 
             $attributs = array_merge($attributs, ['organisationId' => $organisation->id]);
 
-            throw new Exception("Evaluation de gouvernance : ". json_encode($attributs), 500);
-
             if (($soumission = $evaluationDeGouvernance->soumissionFactuel($organisation->id)->first()) && $soumission->statut) {
                 return response()->json(['statut' => 'success', 'message' => "Quota des soumissions atteints", 'data' => ['terminer' => true, 'soumission' => new SoumissionFactuelResource($soumission)], 'statutCode' => Response::HTTP_PARTIAL_CONTENT], Response::HTTP_PARTIAL_CONTENT);
             }
 
             $soumission  = $this->repository->getInstance()->where("evaluationId", $evaluationDeGouvernance->id)->where("organisationId", $organisation->id)->where("formulaireFactuelId", $formulaireDeGouvernance->id)->first();
 
+
+            throw new Exception("Evaluation de gouvernance : ". json_encode($attributs) . ". Soumission : ". $soumission, 500);
             if ($soumission == null) {
                 $soumission = $this->repository->create($attributs);
             } else {
