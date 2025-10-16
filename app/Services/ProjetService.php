2dbd9d8b5370c8f0c5fb300e38da4a53c3039fe2
HÔKWÈ Holding <hokweholding@gmail.com>
2025-10-16 09:39:59 +0200

    Fix bug dans la regle de validation pour la modification d'une composante

--- app/Services/ProjetService.php
+++ app/Services/ProjetService.php
@@ -353,6 +353,7 @@ class ProjetService extends BaseService implements ProjetServiceInterface
                     // Calculer la somme des budgets déjà alloués aux autres organisations de ce fond
                     $totalBudgetAlloue = DB::table('fond_organisations')
                         ->where('fondId', $fond->id)
+                        ->where('organisationId', '!=', $projet->organisation->id)
                         ->whereNull('deleted_at')
                         ->sum('budgetAllouer');
 
