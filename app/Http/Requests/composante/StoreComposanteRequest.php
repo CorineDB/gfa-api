69cce8b213663f393bc53097d5c4775034ebbadf
HÔKWÈ Holding <hokweholding@gmail.com>
2025-11-30 21:08:11 +0100

    commit

--- app/Http/Requests/composante/StoreComposanteRequest.php
+++ app/Http/Requests/composante/StoreComposanteRequest.php
@@ -37,7 +37,7 @@ class StoreComposanteRequest extends FormRequest
                 $projet = $composante ? $composante->projet : null;
             }
 
-            dump($user->role);
+            dump($user->uniteDeGestion);
             dd($user->profilable);
             // Vérifier si le projet appartient à l'utilisateur (organisation ou UG)
             if($projet) {
