4b9334d4597ba8e483a208c67fac3965244ba504
HÔKWÈ Holding <hokweholding@gmail.com>
2025-12-02 11:28:13 +0100

    Commit

--- database/migrations/2025_12_02_102000_edit_survey_forms_unique_constraint.php
+++ database/migrations/2025_12_02_102000_edit_survey_forms_unique_constraint.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
-class UpdateUniqueConstraintForSurveyFormsTable extends Migration
+class EditUniqueConstraintForSurveyForms extends Migration
 {
     /**
      * Run the migrations.
