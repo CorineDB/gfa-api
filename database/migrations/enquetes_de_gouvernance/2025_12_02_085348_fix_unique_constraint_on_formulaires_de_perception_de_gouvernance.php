a0b06309d168bff4baa97b5666d443186b13c29f
HÔKWÈ Holding <hokweholding@gmail.com>
2025-12-02 10:02:55 +0100

    Commit

---
+++ database/migrations/enquetes_de_gouvernance/2025_12_02_085348_fix_unique_constraint_on_formulaires_de_perception_de_gouvernance.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixUniqueConstraintOnFormulairesFactuelDeGouvernance extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('formulaires_de_perception_de_gouvernance', function (Blueprint $table) {
+            // 🔄 Restaurer l'unique si rollback
+            $table->unique('libelle', 'formulaires_de_perception_de_gouvernance_libelle_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
